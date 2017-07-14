e1c87a246dc172d47f64ffa3002ca0849a0088a8
vojinpu <vojinpupavac@gmail.com>
2017-07-14 19:58:28 +0530

    Function for finding right enum works

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+
+public enum Car {
+	   lamborghini(900),tata(2),audi(50),fiat(15),honda(12);
+	   private int price;
+	   Car(int p) {
+	      price = p;
+	   }
+	   int getPrice() {
+	      return price;
+	   } 
+}
+
+
